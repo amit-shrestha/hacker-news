0475c91405b4da51e4ecfd1d2594dfa8ac6f71de
amitshrestha <meamitshrestha000@gmail.com>
2019-01-28 17:02:50 +0545

    Updates

---
+++ precache-manifest.cb1234cf0e65bbb525d922334420f52a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a4afb314ad17d5dbda6b",
+    "url": "/hacker-news/static/js/runtime~main.a4afb314.js"
+  },
+  {
+    "revision": "37649747d5f7f28e1102",
+    "url": "/hacker-news/static/js/main.37649747.chunk.js"
+  },
+  {
+    "revision": "10896950260a4fbcc3ad",
+    "url": "/hacker-news/static/js/1.10896950.chunk.js"
+  },
+  {
+    "revision": "37649747d5f7f28e1102",
+    "url": "/hacker-news/static/css/main.2a5e2b40.chunk.css"
+  },
+  {
+    "revision": "79190f7f71303a6f93623fc8d263b082",
+    "url": "/hacker-news/index.html"
+  }
+];
